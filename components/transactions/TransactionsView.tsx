e6eadf76bddfb9c07479472b4b284c4ed8f4ecfb
nrivera <noeriverap99@gmail.com>
2026-06-27 17:52:21 -0600

    refactor: query the state where transactions created in "My Transactions" are stored

--- components/transactions/TransactionsView.tsx
+++ components/transactions/TransactionsView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import { useTransactions } from "@/hooks/useTransactions";
 import TransactionsTabs from "./TransactionTabs";
@@ -8,6 +8,7 @@ import TransactionsTable from "./TransactionTable";
 import TransactionSkeleton from "./TransactionSkeleton";
 import TransactionEmpty from "./TransactionEmpty";
 import { useUserStore } from "@/store/user.store";
+import { useTransferStore } from "@/store/transfer.store";
 
 export default function TransactionsView() {
   const [tab, setTab] = useState("movements");
@@ -16,15 +17,28 @@ export default function TransactionsView() {
   const transactionQueries = useTransactions(
     user?.products.map((product) => product.id) ?? [],
   );
+  const localTransactions = useTransferStore((state) => state.transactions);
 
   const hasQueries = transactionQueries.length > 0;
 
   const isPending =
     !hasQueries || transactionQueries.some((query) => query.isPending);
 
-  const transactions = transactionQueries.flatMap(
-    (query) => query.data?.items ?? [],
+  const apiTransactions = useMemo(
+    () => transactionQueries.flatMap((query) => query.data?.items ?? []),
+    [transactionQueries],
   );
+
+  const transactions = useMemo(() => {
+    const merged = [...localTransactions, ...apiTransactions];
+
+    return merged.filter(
+      (transaction, index, array) =>
+        array.findIndex(
+          (item) => item.transaction_number === transaction.transaction_number,
+        ) === index,
+    );
+  }, [localTransactions, apiTransactions]);
 
   return (
     <section className="space-y-6">
